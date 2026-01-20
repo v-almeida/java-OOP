375e44f5cacf76be4b6eab35feec9be6916d1841
v-almeida <almeida.vitor20000@gmail.com>
2026-01-20 19:25:36 -0300

    Livro class included

---
+++ java-OOP/src/Livro.java
@@ -0,0 +1,13 @@
+/*Cenário 1: Crie uma classe que represente um Livro de uma biblioteca. Em outra classe
+(Programa), instancie os seguintes livros e apresente seus dados na tela. (Não precisa usar
+Model-View-Controller MVC) */
+
+public class Livro {
+    
+    String codigo;
+    String titulo;
+    String autores[];
+    String ISBN;
+    int ano;
+    
+}
